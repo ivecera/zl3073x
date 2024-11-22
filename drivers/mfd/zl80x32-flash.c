8a90195db21e9c8eb5d178199d2e434471c8f6d8
Ivan Vecera <ivecera@redhat.com>
2024-11-22 18:16:44 +0100

    mfd: zl80x32: Add dummy devlink flash callback
    Provide a dummy devlink flash callback, the full functionality is added
    by subsequent patches from this series.
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

---
+++ drivers/mfd/zl80x32-flash.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "zl80x32.h"
+
+/**
+ * zl80x32_flash_update - Devlink flash update callback
+ * @devlink: devlink structure pointer
+ * @params: flashing parameters pointer
+ * @extack: netlink ext ack pointer to report errors
+ *
+ * Returns 0 in case of success or negative value otherwise
+ */
+int zl80x32_flash_update(struct devlink *devlink,
+			 struct devlink_flash_update_params *params,
+			 struct netlink_ext_ack *extack)
+{
+	devlink_flash_update_status_notify(devlink, "Preparing to flash",
+					   params->component, 0, 0);
+
+	devlink_flash_update_status_notify(devlink, "Flashing done",
+					   params->component, 0, 0);
+
+	return 0;
+}
--- drivers/mfd/zl80x32.h
+++ drivers/mfd/zl80x32.h
@@ -4,10 +4,18 @@
 #define __ZL80X32_CORE_H
 
 #include <linux/mfd/zl80x32.h>
+#include <net/devlink.h>
 
 struct zl80x32_dev *zl80x32_dev_alloc(struct device *dev);
 int zl80x32_dev_init(struct zl80x32_dev *zldev);
 void zl80x32_dev_exit(struct zl80x32_dev *zldev);
 const struct regmap_config *zl80x32_get_regmap_config(void);
+
+/*
+ * Misc functions
+ */
+int zl80x32_flash_update(struct devlink *devlink,
+			 struct devlink_flash_update_params *params,
+			 struct netlink_ext_ack *extack);
 
 #endif /* __ZL80X32_CORE_H */
