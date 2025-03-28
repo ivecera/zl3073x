34ed1cf04ab27cd720ce9cdd6360752a2ca175ad
Ivan Vecera <ivecera@redhat.com>
2025-03-28 14:56:53 +0100

    dpll: zl3073x: Implement phase offset monitor feature
    Implement phase offset monitor feature to allow a user to monitor
    phase offsets across all available inputs.
    
    The device firmware periodically performs phase offsets measurements for
    all available DPLL channels and input references. The driver can ask
    the firmware to fill appropriate latch registers with measured values.
    
    There are 2 sets of latch registers for phase offsets reporting:
    
    1) DPLL-to-connected-ref: up to 5 registers that contain values for
       phase offset between particular DPLL channel and its connected input
       reference.
    2) selected-DPLL-to-ref: 10  registers that contain values for phase
       offsets between selected DPLL channel and all available input
       references.
    
    Both are are filled with single read request so the driver can read
    DPLL-to-connected-ref phase offset for all DPLL channels at once.
    This was implemented in the previous patch.
    
    To read selected-DPLL-to-ref registers for all DPLLs a separate read
    request has to be sent to device firmware for each DPLL channel.
    
    To implement phase offset monitor feature:
    * Extend zl3073x_ref_phase_offsets_update() to select given DPLL channel
      in phase offset read request. The caller can set channel==-1 if it
      will not read Type2 registers.
    * Use this extended function to update phase offset latch registers
      during zl3073x_dpll_changes_check() call if phase monitor is enabled
    * Extend zl3073x_dpll_pin_phase_offset_check() to check phase offset
      changes for all available input references
    * Extend zl3073x_dpll_input_pin_phase_offset_get() to report phase
      offset values for all available input references
    * Implement phase offset monitor callbacks to enable/disable this
      feature
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/dpll/zl3073x/dpll.h
+++ drivers/dpll/zl3073x/dpll.h
@@ -27,6 +27,7 @@ struct zl3073x_dpll {
 	u8				refsel_mode;
 	u8				forced_ref;
 	u8				check_count;
+	bool				phase_monitor;
 	struct dpll_device		*dpll_dev;
 	enum dpll_lock_status		lock_status;
 	struct list_head		pins;
